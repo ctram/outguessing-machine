18384c78374589b8f5e467d52bdbe0b63d4e4366
Chris Tram <chris@mytime.com>
2019-03-03 15:30:48 -0800

    Update Inputs.js
    formatting

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -2,15 +2,32 @@ import React, { Component } from 'react';
 
 export class Inputs extends Component {
     render() {
-        let { gameInSession } = this.props;
+        let { gameInSession, handleHumanAction } = this.props;
 
-        return <div className={"inputs d-flex flex-row justify-content-center" + (gameInSession ? '' : ' d-none')}>
-            <button className="btn btn-primary">
-                1
-            </button>
-            <button className="btn btn-primary">
-                0
-            </button>
-        </div>
+        return (
+            <div
+                className={
+                    'inputs d-flex flex-row justify-content-center' +
+                    (gameInSession ? '' : ' d-none')
+                }
+            >
+                <button
+                    className="btn btn-primary"
+                    onClick={() => {
+                        handleHumanAction(true);
+                    }}
+                >
+                    1
+                </button>
+                <button
+                    className="btn btn-primary"
+                    onClick={() => {
+                        handleHumanAction(false);
+                    }}
+                >
+                    0
+                </button>
+            </div>
+        );
     }
-};
+}
