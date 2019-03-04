76fd0b0f98c50fe70292d95832731550956951a3
Chris Tram <chris@mytime.com>
2019-03-03 16:31:25 -0800

    Update strategies.js
    some name changes;

--- src/lib/strategies.js
+++ src/lib/strategies.js
@@ -4,23 +4,58 @@ class MethodNotImplementedError {
     }
 }
 
+class NotEnoughPriorMovesError {
+    constructor() {
+        this.message = 'Not enough prior human moves to use strategy.';
+    }
+}
+
 export class Strategy {
     humanNextInput() {
         throw new MethodNotImplementedError();
     }
+
+    invertMoves(moves) {
+        return moves.map(move => {
+            return !move;
+        });
+    }
+
+    guessNextMove(humanPriorMoves) {
+        let subsetHumanPriorMoves = this.subsetHumanPriorMoves(humanPriorMoves);
+
+        let res = subsetHumanPriorMoves.every((move, idx) => {
+            return move === subsetHumanPriorMoves[idx];
+        });
+
+        if (res) {
+            return true;
+        }
+
+        res =  this.invertMoves(subsetHumanPriorMoves).every((move, idx) => {
+            return move === subsetHumanPriorMoves[idx];
+        });
+
+        if (res) {
+            return false;
+        }
+
+        return null;
+    }
+
+    subsetHumanPriorMoves(humanPriorMoves) {
+        if (humanPriorMoves.length < this.lengthOfPattern) {
+            throw new NotEnoughPriorMovesError();
+        }
+
+        humanPriorMoves.slice(this.lengthOfPattern * -1, humanPriorMoves.length);
+    }
 }
 
 export class StrategyTwoInARow extends Strategy {
-    humanNextInput(humanLastMoves) {
-        if (humanLastMoves.length < 2) {
-            return;
-        }
-
-        let a = humanLastMoves[humanLastMoves.length - 2];
-        let b = humanLastMoves[humanLastMoves.length - 1];
-
-        if (a === b) {
-            return !a;
-        }
+    constructor() {
+        super();
+        this.pattern = [true, true];
+        this.nextGuess = false;
     }
 }
