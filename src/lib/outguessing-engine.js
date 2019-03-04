166526dacfa4fc5cebf0823839e8cb2f8cf0c9ad
Chris Tram <chris@mytime.com>
2019-03-03 16:04:04 -0800

    revise how strategy is chosen; we first match the pattern the human is using; based on that pattern, we find the matching strategy, then ask the strategy what the next guess will be

--- src/lib/outguessing-engine.js
+++ src/lib/outguessing-engine.js
@@ -1,35 +1,45 @@
+import { StrategyTwoInARow } from "./strategies";
+
+const STRATEGIES = [new StrategyTwoInARow()];
+
+
 export class OutguessingEngine {
-    constructor(strategies = []) {
+    constructor() {
         this.humanLastMoves = [];
-        this.strategies = [].concat(strategies);
+        this.strategies = STRATEGIES;
         this.score = { computer: 0, human: 0 };
     }
 
+    guessRandomly() {
+        return Math.round(Math.random() * 1);
+    }
+
     guessHumansNextInput() {
-        let winningStrategy = null;
+        let matchingStrategy = this.findMatchingStrategy(this.humanLastMoves);
 
+        if (matchingStrategy) {
+            return matchingStrategy.guessHumansNextInput(this.humanLastMoves);
+        }
+        
+        return this.guessRandomly();
+    }
+
+    rememberHumanInput(humanChoice) {
+        this.humanLastMoves.push(humanChoice);
+    }
+
+    findMatchingStrategy(humanLastMoves) {
+        let matchingStrategy = null;
+        
         this.strategies.some(strategy => {
-            winningStrategy = strategy.humanNextInput(this.humanLastMoves);
-
-            if (winningStrategy !== undefined && winningStrategy !== null) {
+            if (strategy.patternRegconized(humanLastMoves)) {
+                matchingStrategy = strategy;
                 return true;
             }
 
             return false;
         });
 
-        if (winningStrategy !== undefined && winningStrategy !== null) {
-            return winningStrategy;
-        }
-
-        return Math.round(Math.random() * 1);
-    }
-
-    rememberHumanInput(input) {
-        this.humanLastMoves.push(input);
-
-        this.strategies.forEach(strategy => {
-            strategy.rememberHumanInput(input);
-        });
+        return matchingStrategy;
     }
 }
